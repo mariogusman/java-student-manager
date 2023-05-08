48ae4fb2d1d242227afcbf4c247b9554b47f1d86
Mario Gusman <mariogusman@gmail.com>
2023-05-08 14:46:02 +0100

    Course Enum boilerplate

---
+++ src/ie/atu/sw/Course.java
@@ -0,0 +1,31 @@
+package ie.atu.sw;
+
+public enum Course {
+	SOFT_DEV 	("Software Development", 	"Computing"), 
+	DATA_ANA 	("Data Analytics", 			"Computing"), 
+	WEB_DEV 	("Web Development", 		"Computing"), 
+	CHEM 		("Chemistry", 				"Science"), 
+	PHYS 		("Physics", 				"Science"), 
+	MEDI 		("Medicine", 				"Medicine"), 
+	ACC 		("Accounting", 				"Commerce");
+
+	private final String cname;
+	private final String cfaculty;
+	
+	Course(String name, String faculty) {
+		this.cname = name;
+		this.cfaculty = faculty;
+	}
+
+	public String getCourseName() {
+		return cname;
+	}
+
+	public String getFaculty() {
+		return cfaculty;
+	}
+
+	
+
+	
+}
