e65798ef50cb4e9284994ab89ac556e0c16e0684
Mario Gusman <mariogusman@gmail.com>
2023-05-08 14:45:50 +0100

    Address.java boilerplate

---
+++ src/ie/atu/sw/Address.java
@@ -0,0 +1,14 @@
+package ie.atu.sw;
+
+public class Address {
+	private String address;
+
+	public Address(String address) {
+		super();
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+}
