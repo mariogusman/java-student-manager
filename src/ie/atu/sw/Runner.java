8e1f3c6c1002fba305be33a7e806bab6e63302eb
Mario Gusman <mariogusman@gmail.com>
2023-05-08 15:22:40 +0100

    Create Runner.java

---
+++ src/ie/atu/sw/Runner.java
@@ -0,0 +1,12 @@
+package ie.atu.sw;
+
+public class Runner {
+	public static void main(String[] args) {
+		StudentManager sm = new StudentManager();
+		Student s = null;
+		
+		sm.add(s);
+		sm.delete(s.sid());
+		
+	}
+}
